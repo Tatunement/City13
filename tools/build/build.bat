2ff205d73ba18b805ee59e541335c35f33eb6a04
Tatunement <magmasevenchannel@gmail.com>
2024-03-05 20:53:20 +0100

    removed next_map.json and set City13 as main map

--- tools/build/build.bat
+++ tools/build/build.bat
@@ -3,4 +3,7 @@ REM deleting the file tgstation.dmb automatically that way you don't have to do 
 if exist tgstation.dmb (
   del tgstation.dmb 2>nul
 )
+if exist data/next_map.json (
+  del data/next_map.json 2>nul
+)
 "%~dp0\..\bootstrap\node.bat" --experimental-modules "%~dp0\build.js" %*
